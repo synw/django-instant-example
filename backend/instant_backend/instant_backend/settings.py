cfb91fafab524bbfd593fb432ed762bb1802875f
synw <synwx@protonmail.com>
2022-12-10 11:51:54 +0100

    Update the example with latest packages and add a template example

--- backend/instant_backend/instant_backend/settings.py
+++ backend/instant_backend/instant_backend/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "instant_backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
